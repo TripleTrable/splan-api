31729bd54181e5319f21cebcbae1d47870a3b181
Lars Niesen <lars.niesen@mailbox.org>
2024-03-25 17:17:17 +0100

    run clang-format

--- src/splan/json/spapi.h
+++ src/splan/json/spapi.h
@@ -8,10 +8,10 @@
     RESPONSE: [[{"id":3,"shortname":"*RO","name":"Rosenheim (Hochschule)"},{"id":6,"shortname":"AÖ","name":"Altötting (Berufliche Oberschule Inn-Salzach)"},{"id":8,"shortname":"BGL","name":"Ainring (Akademie Berchtesgadener Land)"},{"id":7,"shortname":"Bgh","name":"cb - Hochschulinstitut Burghausen"},{"id":9,"shortname":"CC","name":"Campus Chiemgau Traunstein"},{"id":4,"shortname":"MÜ","name":"Mühldorf (Haus der Wirtschaft)"},{"id":5,"shortname":"WS","name":"Wasserburg (RoMed Klinik)"}]]
 */
 
-loc* splan_get_locs(void);
+loc *splan_get_locs(void);
 
 //local
-loc* locs_parse_json(const char *data);
+loc *locs_parse_json(const char *data);
 char *iso8859_1_to_utf_8(char *iso);
 
 #endif // !SPAPI_H
--- src/splan/splan_init.c
+++ src/splan/splan_init.c
@@ -7,16 +7,16 @@
 server_user_config _server_config;
 int splan_init(server_user_config *config)
 {
-	int endpoint_length = strlen(config->server_api_endpoint) + 1;
+    int endpoint_length = strlen(config->server_api_endpoint) + 1;
 
-	_server_config.server_api_endpoint = malloc(endpoint_length);
-	if (_server_config.server_api_endpoint == NULL) {
-		perror("Could not allocate memory for the api_endpoint string\n");
-		return -1;
-	}
-	strncpy(_server_config.server_api_endpoint, config->server_api_endpoint,
-		endpoint_length);
+    _server_config.server_api_endpoint = malloc(endpoint_length);
+    if (_server_config.server_api_endpoint == NULL) {
+        perror("Could not allocate memory for the api_endpoint string\n");
+        return -1;
+    }
+    strncpy(_server_config.server_api_endpoint, config->server_api_endpoint,
+            endpoint_length);
 
-	curl_global_init(CURL_GLOBAL_ALL);
-	return 0;
+    curl_global_init(CURL_GLOBAL_ALL);
+    return 0;
 }
