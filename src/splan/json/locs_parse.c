2b533452be38ec6835090739960c65f08c7307b0
Lars Niesen <lars.niesen@mailbox.org>
2024-03-25 19:16:26 +0100

    test backup

--- src/splan/json/locs_parse.c
+++ src/splan/json/locs_parse.c
@@ -21,31 +21,28 @@
 ]
  */
 
-loc *locs_parse_json(const char *data)
+loc *locs_parse_json(const char *_data)
 {
     loc *ret_val = NULL;
-    printf("---------------\n");
-    printf("%s", data);
-    printf("---------------\n");
-    cJSON *json = cJSON_Parse(data);
+    cJSON *json = cJSON_Parse(_data);
 
     if (json == NULL) {
         const char *error_ptr = cJSON_GetErrorPtr();
         if (error_ptr != NULL) {
             fprintf(stderr, "Error before: %s\n", error_ptr);
-            printf("===============\n");
-            printf("%p\n", error_ptr);
-            printf("%p\n", data);
-            printf("===============\n");
         }
         errno = EINVAL;
         goto cleanup;
     }
 
+    // can use hard coding to access this as the double array is always present
+    //
+    cJSON *data = json->child->child;
+
     printf("===============\n");
-    printf("%s\n", cJSON_Print(json));
+    printf("%s\n", cJSON_Print(data));
     printf("===============\n");
-    printf("%c\n", cJSON_IsArray(json));
+    printf("%s\n", cJSON_Print(data->next));
     printf("===============\n");
 
 cleanup:
