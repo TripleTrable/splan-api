8a8d87ba6a603848d84d55ed2e1e1751610832a9
Lars Niesen <lars.niesen@mailbox.org>
2024-03-25 17:09:47 +0100

    add first draft for getlocs

---
+++ src/splan/json/iso_to_utf.c
@@ -0,0 +1,54 @@
+#include <iconv.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *iso8859_1_to_utf_8(char *iso)
+{
+	iconv_t iconvDesc;
+
+	size_t iconv_value;
+	char *utf8_buffer;
+	size_t len;
+	size_t utf8_len;
+	char *utf8_iter;
+
+	int len_start;
+
+	iconvDesc = iconv_open("UTF-8//TRANSLIT//IGNORE", "ISO−8859-1");
+
+	if (iconvDesc == (iconv_t)-1) {
+		perror("During iconv_open the following error occured");
+		return NULL;
+	}
+
+	len = strlen(iso);
+	if (!len) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	utf8_len = 2 * len;
+	utf8_buffer = calloc(utf8_len, sizeof(char));
+
+	if (!utf8_buffer) {
+		errno = ENOMEM;
+		return NULL;
+	}
+
+	utf8_iter = utf8_buffer;
+	len_start = len;
+
+	iconv_value = iconv(iconvDesc, &iso, &len, &utf8_iter, &utf8_len);
+
+	if (iconv_value == (size_t)-1) {
+		return NULL;
+	}
+
+	if (iconv_close(iconvDesc) != 0) {
+		perror("libicon close failed");
+	}
+
+	return utf8_buffer;
+}
---
+++ src/splan/json/spapi.h
@@ -0,0 +1,17 @@
+#ifndef SPAPI_H
+#define SPAPI_H
+#include "../spdata.h"
+
+/*
+2. "getlocs" (get list of universities):
+    GET https://splan.th-rosenheim.de/splan/json?m=getlocs
+    RESPONSE: [[{"id":3,"shortname":"*RO","name":"Rosenheim (Hochschule)"},{"id":6,"shortname":"AÖ","name":"Altötting (Berufliche Oberschule Inn-Salzach)"},{"id":8,"shortname":"BGL","name":"Ainring (Akademie Berchtesgadener Land)"},{"id":7,"shortname":"Bgh","name":"cb - Hochschulinstitut Burghausen"},{"id":9,"shortname":"CC","name":"Campus Chiemgau Traunstein"},{"id":4,"shortname":"MÜ","name":"Mühldorf (Haus der Wirtschaft)"},{"id":5,"shortname":"WS","name":"Wasserburg (RoMed Klinik)"}]]
+*/
+
+loc* splan_get_locs(void);
+
+//local
+loc* locs_parse_json(const char *data);
+char *iso8859_1_to_utf_8(char *iso);
+
+#endif // !SPAPI_H
